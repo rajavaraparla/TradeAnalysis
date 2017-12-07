dbc225c3821b15db65c57a340eefb596bc305b8d
nvarapar <varaparla.raja@gmail.com>
2017-12-07 16:41:01 +0530

    Load Intra Database

--- Load/LoadIntraData.py
+++ Load/LoadIntraData.py
@@ -3,24 +3,67 @@ Get Intraday data
 
 '''
 
+from conf import config
+from conf import constants
 from utils import trade_utils
 
-def get_intra_15min_data(ticker,days=1):
-    return trade_utils.get_google_finance_intraday(ticker, period=300, days=days)
-
-def get_intra_30min_data(ticker, days=1):
-    return trade_utils.get_google_finance_intraday(ticker, period=1800, days=days)
-
-def get_intra_hour_data(ticker, days=1):
-    return trade_utils.get_google_finance_intraday(ticker, period=3600, days=days)
 
 
 
 if __name__ == "__main__":
     #print(trade_utils.get_google_finance_intraday("CIPLA",period=300,days=1))
-    print(get_intra_15min_data("BAJAJ-AUTO",days=300))
+    # Get last 90 days data.
+    SYMBOLS = constants.ALL_SYMBOLS
+    NUM_DAYS = 90
+
+    for index,ticker in enumerate(SYMBOLS):
+        DF_5MIN = trade_utils.get_intra_5min_data(ticker=ticker, days=NUM_DAYS)
+        RESULT_5MIN = trade_utils.load_db_table(
+            ticker
+            , DF_5MIN
+            , config.DB_NAME
+            , config.DB_HOST
+            , config.DB_USER
+            , config.DB_PASSWORD
+            , config.DB_INTRA_5MIN_TABLE
+        )
+
+        DF_15MIN = trade_utils.get_intra_15min_data(ticker=ticker, days=NUM_DAYS)
+        RESULT_5MIN = trade_utils.load_db_table(
+            ticker
+            , DF_15MIN
+            , config.DB_NAME
+            , config.DB_HOST
+            , config.DB_USER
+            , config.DB_PASSWORD
+            , config.DB_INTRA_15MIN_TABLE
+        )
+
+        DF_30MIN = trade_utils.get_intra_30min_data(ticker=ticker, days=NUM_DAYS)
+        RESULT_5MIN = trade_utils.load_db_table(
+            ticker
+            , DF_30MIN
+            , config.DB_NAME
+            , config.DB_HOST
+            , config.DB_USER
+            , config.DB_PASSWORD
+            , config.DB_INTRA_30MIN_TABLE
+        )
+
+        DF_HOUR = trade_utils.get_intra_hour_data(ticker=ticker, days=NUM_DAYS)
+        RESULT_5MIN = trade_utils.load_db_table(
+            ticker
+            , DF_HOUR
+            , config.DB_NAME
+            , config.DB_HOST
+            , config.DB_USER
+            , config.DB_PASSWORD
+            , config.DB_INTRA_HOUR_TABLE
+        )
+
+
     #print(get_intra_30min_data("BAJAJ-AUTO", days=1))
-    print(get_intra_hour_data("BAJAJ-AUTO", days=1))
+    #print(get_intra_hour_data("BAJAJ-AUTO", days=1))
 
 
 
