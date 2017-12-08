b67bc80c4c04db09dd69d742ff2e66865592fd9b
nvarapar <varaparla.raja@gmail.com>
2017-12-08 16:33:27 +0530

    Separated Base and Derived Classes

--- conf/config.py
+++ conf/config.py
@@ -1,7 +1,14 @@
 '''
 This will contain configuration related defines
 '''
-DB_NAME = "tradedata"
-DB_USER = "taadmin"
-DB_PASSWORD = "Progress!2009"
+DB_NAME = "trade_data"
+DB_USER = "tradeuser"
+DB_PASSWORD = "Trade123#"
 DB_HOST = "localhost"
+
+DB_INTRA_5MIN_TABLE = "intra_data_5min"
+DB_INTRA_15MIN_TABLE = "intra_data_15min"
+DB_INTRA_30MIN_TABLE = "intra_data_30min"
+DB_INTRA_HOUR_TABLE = "intra_data_hour"
+
+DB_EOD_TABLE = 'hist_data'
