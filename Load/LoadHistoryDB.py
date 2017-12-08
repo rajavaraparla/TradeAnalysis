44d3540af59813014b89d9bcb44a6cd2b7cf47ce
nvarapar <varaparla.raja@gmail.com>
2017-12-08 16:34:41 +0530

    Separated Base and Derived Classes

--- Load/LoadHistoryDB.py
+++ Load/LoadHistoryDB.py
@@ -12,21 +12,20 @@ import pymysql
 from sqlalchemy import create_engine, MetaData, TEXT, Integer, Float, Table, Column, ForeignKey, String, BIGINT, DATE
 import matplotlib.pyplot as plt
 from conf import constants, config
-from utils import trade_utils
+from utils import trade_utils, db_utils
 
 
 
 
 if __name__ == '__main__':
     ALL_SYMBOLS = constants.ALL_SYMBOLS
-    #ALL_SYMBOLS = ['MARUTI']
-    TIME_DELTA = 1500
+    TIME_DELTA = 0
     START_DATE = date.today() - timedelta(TIME_DELTA)
     END_DATE = date.today()
     for symbol in ALL_SYMBOLS:
         DF_EOD = trade_utils.get_nse_eoddata(symbol,start_date=START_DATE,end_date=END_DATE)
         #print (DF)
-        RESULT_EOD = trade_utils.load_db_table(
+        RESULT_EOD = db_utils.load_db_table(
             symbol
             , DF_EOD
             , config.DB_NAME
