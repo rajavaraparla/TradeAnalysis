5049bef7b949609a21c4637c675df00d2d37bd93
nvarapar <varaparla.raja@gmail.com>
2017-12-08 16:33:57 +0530

    Separated Base and Derived Classes

--- conf/setup.sql
+++ conf/setup.sql
@@ -1,6 +1,8 @@
  CREATE DATABASE IF NOT EXISTS trade_data;
  CREATE USER IF NOT EXISTS 'tradeuser'@'localhost' IDENTIFIED BY 'Trade123#';
  GRANT ALL ON tradedata.* TO 'tradeuser'@'localhost' IDENTIFIED BY 'Trade123#';
+  GRANT ALL PRIVILEGES ON *.* TO 'tradeuser'@'localhost' IDENTIFIED BY 'Trade123#' WITH GRANT OPTION;
+
 
  use trade_data;
 
@@ -73,3 +75,28 @@ CREATE TABLE IF NOT EXISTS intra_data_hour (
   PRIMARY KEY (ticker,TradeTime)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE IF NOT EXISTS intra_olh_trade (
+  ticker varchar(255) NOT NULL,
+  TradeDate date DEFAULT NULL,
+  TradeTime datetime NOT NULL,
+  Open float DEFAULT NULL,
+  High float DEFAULT NULL,
+  Low float DEFAULT NULL,
+  Close float DEFAULT NULL,
+  PClose float DEFAULT NULL,
+  Atp  float DEFAULT NULL,
+  ipivot float DEFAULT NULL,
+  NSE_Trade varchar(255) ,
+  BSE_Trade varchar(255) ,
+  Entry1 float DEFAULT NULL,
+  Entry2 float DEFAULT NULL,
+  Entry3 float DEFAULT NULL,
+  SL float DEFAULT NULL,
+  Target1 float DEFAULT NULL,
+  Target2 float DEFAULT NULL,
+  Target3 float DEFAULT NULL,
+  Target4 float DEFAULT NULL,
+  Target5 float DEFAULT NULL,
+PRIMARY KEY (ticker,TradeTime)
+)
